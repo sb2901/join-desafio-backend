2c37f98e13170be20cc6273ef6265b40ea1e9a23
sb2901 <34243189+sb2901@users.noreply.github.com>
2024-07-10 09:53:13 -0300

    Authentication (#2)
    * Authentication

--- src/main/java/com/join/test/fullstack/testfullstack/configuration/SecurityConfiguration.java
+++ src/main/java/com/join/test/fullstack/testfullstack/configuration/SecurityConfiguration.java
@@ -1,13 +1,20 @@
 package com.join.test.fullstack.testfullstack.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -17,6 +24,14 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    private RequestFilterConfig requestFilterConfig;
+
+    public static final String [] NO_AUTHENTICATION = {
+            "/users/login",
+            "/users/register"
+    };
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
@@ -37,8 +52,27 @@ public class SecurityConfiguration {
                 .cors(Customizer.withDefaults())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(request -> {
-                    request.anyRequest().permitAll();
-                })
+                    request.requestMatchers(NO_AUTHENTICATION).permitAll();
+                    request
+                            .requestMatchers(HttpMethod.GET, "/category", "/product",
+                                    "/category/{id}", "/product/{id}",
+                                    "/category/list", "/product/list").authenticated()
+                            .requestMatchers(HttpMethod.POST, "/category", "/product").authenticated()
+                            .requestMatchers(HttpMethod.PUT, "/category", "/product").authenticated()
+                            .requestMatchers(HttpMethod.DELETE, "/category", "/product", "/category/{id}", "/product/{id}").authenticated()
+                            .anyRequest().denyAll();
+                } )
+                .addFilterBefore(requestFilterConfig, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/join/test/fullstack/testfullstack/dto/LoginDto.java
@@ -0,0 +1,6 @@
+package com.join.test.fullstack.testfullstack.dto;
+
+public record LoginDto(
+        String email,
+        String password
+) {}
